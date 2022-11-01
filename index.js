5a7b18c2a325a7e986feb7c2301885ab620b9948
Theodore John Josef <john.josef@aol.com>
2022-11-01 15:50:27 -0700

    commit tree 8e844e6c21cb54e9713dad8386f08f253461dda0

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+/*let arr = [1, 2, 3, "a", 4, "b", 5];
+
+function isAString(e) {
+    return typeof e === "string";
+}
+
+arr.find(isAString)
+*/
+
+function superbowlWin (games) {
+    const wonGame = games.find((game) => {
+        return game.result === "W";
+    });
+    if (wonGame) {
+        return wonGame.year;
+    } 
+}
